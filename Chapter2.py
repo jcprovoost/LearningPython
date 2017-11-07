ee6bb619b03a90b950c95740ec93518b1bc4bed3
jcprovoost <33288041+jcprovoost@users.noreply.github.com>
2017-11-07 19:42:49 +0100

    Create Chapter2.py

---
+++ Chapter2.py
@@ -0,0 +1,15 @@
+# LearningPython
+# Jesper Provoost, 2017, CreaTe UT
+
+print("What to tip the restaurant waiter or waitress?")
+
+totalPrice = int(input("What is the total price of the meal?"))
+
+fifteenPerc = int(totalPrice / 100 * 15)
+twentyPerc = int(totalPrice / 100 * 15)
+
+print("\nFeeling satisfied? With a total of €",totalPrice,", a 15% tip would be €" \
+       ,fifteenPerc,"\nIn total this is £",billTotal + fiftenTip)
+
+print("\nIf you're feeling generous then a 20% tip would be £" \
+       ,twentyTip,"\nIn total this is £",billTotal + twentyTip)
